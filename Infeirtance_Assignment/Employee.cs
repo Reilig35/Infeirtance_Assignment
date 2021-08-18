1396a4416520eef4bff5893952c14dbb7837bed1
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-18 12:43:40 +0100

    Add project files.

---
+++ Infeirtance_Assignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheirtance_Assignment
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine($"\n\n\nI.D: {Id}\nName: {FirstName} {LastName}");
+        }
+    }
+}
+
+        
---
+++ Infeirtance_Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Inheirtance_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+
+            person.SayName();
+
+
+            Employee william = new Employee();
+            william.FirstName = "William";
+            william.LastName = "Twomey";
+            william.Id = 546729;
+
+            william.SayName();
+        }
+        
+           
+            
+        
+    }
+}
+
+            
+            
+
+
+            
+
